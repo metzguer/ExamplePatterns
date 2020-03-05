5a90017134ae91be9c041fbce19e5f26f8984e01
Metzguer <metzguer@gmx.es>
2020-03-05 16:48:58 -0600

    Add example Singleton basic

---
+++ Singleton/PatternSingletton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class PatternSingletton
+    {
+        private static PatternSingletton _instance = null ;
+        public string mensaje = "";
+
+        protected PatternSingletton()
+        {
+            mensaje = "Intance unique";
+        }
+
+        public static PatternSingletton instance {
+            get
+            {
+                return (_instance == null) ? new PatternSingletton() : null ; 
+            }
+        }
+
+       
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var msgSingleton = PatternSingletton.instance.mensaje; 
+
+            Console.WriteLine(msgSingleton);
+        }
+    }
+}
